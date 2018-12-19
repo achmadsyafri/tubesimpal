96ce0be182c2ec3ee878e4ee30b8c656b0fe4115
achmadsyafri <achmadsyafri11@gmail.com>
2018-12-19 08:05:50 +0700

    Revert "Update addPembayaran.php"
    This reverts commit fa3c991f02b0b8cc26640b60cc0f4ca1cac0a795.

--- application/views/addPembayaran.php
+++ application/views/addPembayaran.php
@@ -297,7 +297,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.3.7
     </div>
-    <strong>Copyright &copy; 2018-2019 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
     reserved.
   </footer>
 
